9993d349c5c64e6b08c17ec9551a66adcc605b35
Juseong Park <juseong001214@gmail.com>
2023-01-25 18:28:54 +0900

    1번째 커밋

---
+++ src/main/java/polimorphism02/SamsungTV.java
@@ -0,0 +1,29 @@
+package polimorphism02;
+
+public class SamsungTV implements TV { //TV인터페이스에 선언된 내용을 구현
+
+	@Override
+	public void powerOn() {
+		System.out.println("SamsungTV - 전원을 켭니다.");
+
+	}
+
+	@Override
+	public void powerOff() {
+		System.out.println("SamsungTV - 전원을 끕니다.");
+
+	}
+
+	@Override
+	public void volumeUp() {
+		System.out.println("SamsungTV - 소리를 높입니다.");
+
+	}
+
+	@Override
+	public void volumeDown() {
+		System.out.println("SamsungTV - 소리를 낮춥니다.");
+
+	}
+
+}
---
+++ src/main/java/polimorphism03/LgTV.java
@@ -0,0 +1,25 @@
+package polimorphism03;
+
+public class LgTV implements TV { //TV라는 인터페이스를 구현하는 LgTV
+
+	@Override
+	public void powerOn() {
+		System.out.println("LgTV - 전원을 켭니다.");
+	}
+
+	@Override
+	public void powerOff() {
+		System.out.println("LgTV - 전원을 끕니다.");
+	}
+
+	@Override
+	public void volumeUp() {
+		System.out.println("LgTV - 소리를 높입니다.");
+	}
+
+	@Override
+	public void volumeDown() {
+		System.out.println("LgTV - 소리를 낮춥니다.");
+	}
+
+}
